7980d92ecd03654098f06333e007acadd50cc498
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-01-20 21:53:45 +0630

    project-planner setup

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="project">
+    <div class="flexing">
+      <div @click="showDetail = !showDetail">
+        <h5>
+          <b>{{ project.title }}</b>
+        </h5>
+      </div>
+      <div class="text-gray">
+        <i class="fas fa-trash"></i>
+        <i class="fas fa-edit"></i>
+        <i class="fas fa-check"></i>
+      </div>
+    </div>
+    <div>
+      <p v-if="showDetail">{{ project.detail }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css");
+@import url("https://use.fontawesome.com/releases/v5.15.1/css/all.css");
+.project {
+  padding: 20px;
+  background: #f2f2f2;
+  margin: 10px;
+  border-left: 5px solid red;
+  border-radius: 5px;
+}
+
+h5 {
+  cursor: pointer;
+  color: indigo;
+}
+i {
+  margin-left: 15px;
+  cursor: pointer;
+}
+i:hover {
+  color: #777;
+}
+.text-gray {
+  color: #444;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.projects = data;
+      })
+      .catch((err) => {
+        console.log(err.message());
+      });
+  },
+};
 </script>
